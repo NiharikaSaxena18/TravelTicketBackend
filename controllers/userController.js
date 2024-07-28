767ba2d0a53227cecefabe4e41d085bbdc1c74bd
Akash <akashtripathiak04@gmail.com>
2024-07-28 17:05:32 +0530

    Updated userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,73 +3,63 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
-// login User
+//login user
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await userModel.findOne({ email });
+
     if (!user) {
-      return res.json({ success: false, message: "User Does Not Exists" });
+      return res.json({ success: false, message: "User does not exist" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid credentials" });
     }
-
-    const token = createToken(user._id);
-    res.json({ success: true, token });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, message: "Login successful", token: token });
   } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: "error" });
+    res.json({ success: false, message: "Login failed" });
   }
 };
 
-//token
-const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JwT_SECRET);
+//generating token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
-// register user
+//register user
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    //user already exists ?
+    //checking whether user exist or not
     const exists = await userModel.findOne({ email });
     if (exists) {
-      return res.json({ success: false, message: "User Already Exists" });
+      return res.json({ success: false, message: "User already exists" });
     }
     //validating email format and strong password
     if (!validator.isEmail(email)) {
-      return res.json({
-        success: false,
-        message: "Please enter a valid email",
-      });
-    }
-    if (password.length < 5) {
-      return res.json({
-        success: false,
-        message: "Please enter a strong password",
-      });
+      return res.json({ success: false, message: "Invalid email" });
     }
 
     //hashing user password
     const salt = await bcrypt.genSalt(8);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new userModel({
+    //creating user
+    const newUser = await userModel({
       name: name,
       email: email,
       password: hashedPassword,
     });
 
     const user = await newUser.save();
-    const token = createToken(user._id);
-    res.json({ success: true, token });
+    const token = generateToken(user._id);
+    res.json({ message: "Successfully Registered", success: true, token });
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: "error" });
+    res.json({ success: false, message: "Some error occurred" });
   }
 };
 
